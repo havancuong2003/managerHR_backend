4c65f86cff85473554d8bc52b8843b3fe2e8aa5d
havancuong2003 <havancuong07122003@gmail.com>
2025-02-26 00:30:38 +0700

    init project

---
+++ src/server.js
@@ -0,0 +1,25 @@
+import dotenv from "dotenv";
+import express from "express";
+import cors from "cors";
+import bodyParser from "body-parser";
+import mongoose from "mongoose";
+import path from "path";
+import morgan from "morgan";
+
+import dbConnect from "./config/dbConnect.js";
+
+dotenv.config();
+
+const app = express();
+
+app.use(cors());
+app.use(bodyParser.json());
+app.use(morgan("dev"));
+
+const PORT = process.env.PORT || 9999;
+const HOST = process.env.HOST || "localhost";
+
+app.listen(PORT, HOST, () => {
+    dbConnect();
+    console.log(`Server running on http://${HOST}:${PORT}`);
+});
