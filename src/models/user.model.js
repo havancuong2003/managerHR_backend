b9a214fba42d0702c40a246053f3827abef27974
havancuong2003 <havancuong07122003@gmail.com>
2025-02-28 19:18:44 +0700

    fix: fix code register, upload avatar to cloud

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,19 +1,22 @@
 import mongoose from "mongoose";
 
-const userSchema = new mongoose.Schema({
-    username: {
-        type: String,
-        required: true,
+const userSchema = new mongoose.Schema(
+    {
+        fullName: { type: String, required: true },
+        dob: { type: String, required: true },
+        gender: { type: String, enum: ["Nam", "Nữ"], required: true },
+        address: { type: String, required: true },
+        phone: { type: String, required: true, unique: true },
+        department: { type: String, required: true },
+        position: { type: String, required: true },
+        salary: { type: Number, required: true },
+        startDate: { type: String, required: true },
+        password: { type: String, required: true },
+        avatarUrl: { type: String },
+        roleId: { type: mongoose.Schema.Types.ObjectId, ref: "Role" },
     },
-    email: {
-        type: String,
-        required: true,
-    },
-    password: {
-        type: String,
-        required: true,
-    },
-});
+    { timestamps: true }
+);
 
 const User = mongoose.model("User", userSchema);
 export default User;
