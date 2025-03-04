f91574b1280e060702510cb9cd67c32d477f9e6f
havancuong2003 <havancuong07122003@gmail.com>
2025-03-04 21:05:36 +0700

    fix: fix employee, fix authenticate

--- src/routes/employee.route.js
+++ src/routes/employee.route.js
@@ -1,5 +1,8 @@
 import express from "express";
-import { getEmployees } from "../controllers/employee.controller.js";
+import {
+    getEmployees,
+    getEmployeeById,
+} from "../controllers/employee.controller.js";
 import {
     authMiddleware,
     roleMiddleware,
@@ -8,5 +11,10 @@ import {
 const router = express.Router();
 
 router.get("/", authMiddleware, roleMiddleware(["admin"]), getEmployees);
-
+router.get(
+    "/:id",
+    authMiddleware,
+    roleMiddleware(["employee"]),
+    getEmployeeById
+);
 export default router;
